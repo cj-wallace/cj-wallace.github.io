b559244e846e3b73f0a252d21a1e2140057f1b7e
Connor Wallace <connorjwallace@hotmail.com>
2020-11-08 00:20:31 -0500

    Project window now opens up and displays info based on projects.json

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -10,12 +10,60 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 for(var i = 0; i < btns.length; i++){
-	console.log(btns[i].id);
 	btns[i].onclick = function() {
 		var jsonObject = getJSON(this.id);
-		console.log(jsonObject);
-		modal.style.display = "block";
+		buildModal(jsonObject);
 	}
+}
+
+function buildModal(jsonObject){
+	//Header
+	var nameHeader = document.getElementById("modal-project-name");
+	nameHeader.innerText = jsonObject.name;
+	
+	//Image
+	var image = document.getElementById("modal-project-image");
+	if (typeof jsonObject.image !== 'undefined'){
+		image.setAttribute("style", "background-image:url('" + jsonObject.image +"');");
+		image.style.display = "block";
+	}
+	else{
+		image.style.display = "none";
+	}
+	
+	//Description
+	var descriptionBlock = document.getElementById("modal-project-desc");
+	descriptionBlock.innerHTML = "";
+	for(var i = 0; i < jsonObject.desc.length; i++){
+		descriptionBlock.innerHTML += "<p>"+jsonObject.desc[i]+"</p>";
+	}
+	
+	//Github Button Link
+	var githubButton = document.getElementById("modal-project-link");
+	githubButton.href = jsonObject.link;
+	
+	//Tags
+	var tagBlock = document.getElementById("modal-project-tags");
+	tagBlock.innerHTML = "";
+	for(var i = 0; i < jsonObject.tags.length; i++){
+		tagBlock.innerHTML += "<a>"+jsonObject.tags[i]+"</a>";
+		if(i == jsonObject.tags.length - 1){
+			tagBlock.innerHTML += "<br>"
+		}
+	}
+	
+	//Date
+	var date = document.getElementById("modal-project-date");
+	date.innerText = jsonObject.date;
+	
+	//Progress Bar
+	var progressBar = document.getElementById("modal-project-progress");
+	progressBar.setAttribute("aria-valuenow", jsonObject.progress);
+	progressBar.setAttribute("style", "width: " + jsonObject.progress +"%;");
+	progressBar.innerText = jsonObject.progress + "%";
+	
+	//Display Modal
+	modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -31,7 +79,7 @@ window.onclick = function(event) {
 }
 
 
-function getJSON(projectName) {
+function getJSON(projectID) {
 	var request = new XMLHttpRequest();
 	request.open("GET", "/projects/projects.json", false);
 	request.send(null)
@@ -40,7 +88,7 @@ function getJSON(projectName) {
 	
 	// Find matching projectName
 	for (var i = 0; i < jsonObject.length; i++){
-	  if (jsonObject[i].name == projectName){
+	  if (jsonObject[i].id == projectID){
 		  return(jsonObject[i]);
 	  }
 	}
